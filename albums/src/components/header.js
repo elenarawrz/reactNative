de0f840c7c842b7a25ec5462e683c4e496ebb1f4
elenavalle <Elena@MacBook-Pro-4.local>
2017-03-30 15:33:13 -0600

    added header

---
+++ albums/src/components/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { text, view } = styles;
+  return (
+    <View style={view}>
+      <Text style={text}>{props.text}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  text: {
+    fontSize: 25,
+    color: '#FFF'
+  },
+  view: {
+    backgroundColor: '#BBB',
+    padding: 10,
+    height: 70,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.7,
+    elevation: 10,
+    position: 'relative'
+  }
+};
+
+export default Header;
