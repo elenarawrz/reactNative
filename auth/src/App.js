8841ab1f5d9cacfd962322dff7a6a374209fd15d
elenavalle <Elena@MacBook-Pro-4.local>
2017-03-31 14:28:09 -0600

    added firebase init

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDfgtnlo3FHtgnDJgl2y73aatCFsS9JroE',
+      authDomain: 'auth-72ef6.firebaseapp.com',
+      databaseURL: 'https://auth-72ef6.firebaseio.com',
+      projectId: 'auth-72ef6',
+      storageBucket: 'auth-72ef6.appspot.com',
+      messagingSenderId: '1046126670418'
+    });
+  }
 
   render() {
     return (
