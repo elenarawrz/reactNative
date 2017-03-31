e85b2f6b5dc67c7e72608b839ece1a1ac6dfc7e5
elenavalle <Elena@MacBook-Pro-4.local>
2017-03-31 16:24:18 -0600

    added primitive TextInput

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { TextInput } from 'react-native';
 import { Card, CardSection, Button } from './common';
 
 class LoginForm extends Component {
+  state = { text: '' };
+  onChangeText = text => {
+    this.setState({ text });
+    console.log(this.state);
+  };
+
   render() {
     return (
       <Card>
         <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={this.onChangeText}
+            style={styles.text}
+          />
         </CardSection>
 
         <CardSection>
@@ -18,5 +30,12 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  text: {
+    height: 50,
+    width: 100
+  }
+};
 
 export default LoginForm;
