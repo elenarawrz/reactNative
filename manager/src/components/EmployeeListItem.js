a75517113c849efaf24b24360f8f0c95739992d4
elenavalle <elenavaller@hotmail.com>
2017-05-10 11:53:58 -0500

    clickable list items

--- manager/src/components/EmployeeListItem.js
+++ manager/src/components/EmployeeListItem.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class EmployeeListItem extends Component {
+  onRowPress() {
+    const { employee } = this.props;
+    Actions.employeeCreate({ employee });
+  }
+
   render() {
     return (
-      <CardSection>
-        <Text style={styles.title}>{this.props.employee.name}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.title}>
+              {this.props.employee.name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
